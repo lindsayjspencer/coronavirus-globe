ffe367b9e8a1a8b31ef73a8944b80031d74fe7fe
Lindsay Spencer <lindsayjspencer@gmail.com>
2020-07-21 21:45:42 +1000

    Add symlinks and file structure bash

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-folder='globe'
+folder=$1
+
+sudo mkdir /var/www/"$folder"/assets/vendor/tween.js
+sudo chown www-data:www-data /var/www/"$folder"/assets/vendor/tween.js
+sudo chmod 777 /var/www/"$folder"/assets/vendor/tween.js
 sudo ln -s /var/www/"$folder"/node/node_modules/tween.js/src/Tween.js /var/www/"$folder"/assets/vendor/tween.js/tween.js
+
 sudo ln -s /var/www/"$folder"/node/node_modules/three/ /var/www/"$folder"/assets/vendor/three
 sudo ln -s /var/www/"$folder"/node/node_modules/jquery/dist/ /var/www/"$folder"/assets/vendor/jquery
 sudo ln -s /var/www/"$folder"/node/node_modules/bootstrap/dist/ /var/www/"$folder"/assets/vendor/bootstrap
 sudo ln -s /var/www/"$folder"/node/node_modules/@popperjs/core/dist/esm/ /var/www/"$folder"/assets/vendor/popperjs
-sudo ln -s /home/user/Node/coronavirus/coronavirus-data.csv /var/www/"$folder"/data/coronavirus-data.csv
+
+sudo chmod -R 777 /var/www/"$folder"/assets
+sudo chown -R www-data:www-data /var/www/"$folder"/assets
