e949c1c9220428413b745a65483aa779ea015d9a
Lindsay Spencer <lindsayjspencer@gmail.com>
2020-07-22 22:27:49 +1000

    Update three globe source

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		<script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
 		<script src="/assets/vendor/tween.js/tween.js"></script>
 		<script src="/assets/vendor/three/build/three.min.js"></script>
-		<script src="/assets/vendor/three-globe-master/src/three-globe.min.js"></script>
+		<script src="//unpkg.com/three-globe"></script>
 		<style>
             @media only screen and (min-width: 578px) {
 				.open-menu {
