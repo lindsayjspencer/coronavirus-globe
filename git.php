cd377c67fad056868a7bc2fdf4dbf9f9b6bc636d
Lindsay Spencer <lindsayjspencer@gmail.com>
2020-07-23 19:01:49 +1000

    PHP formatting

--- git.php
+++ git.php
@@ -1,9 +1,8 @@
-<?
+<?PHP
 
 //Something to write to txt log
 $log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
         "Payload: ".print_r($_POST).PHP_EOL.
-        "User: ".$username.PHP_EOL.
         "-------------------------".PHP_EOL;
 //Save string to log, use FILE_APPEND to append.
 file_put_contents('./log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
