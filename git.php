fd16489a88550f9e3d5f71b1b2bb09ba99f7e86d
Lindsay Spencer <lindsayjspencer@gmail.com>
2020-07-23 20:27:57 +1000

    Test

--- git.php
+++ git.php
@@ -10,48 +10,49 @@
 // script errors will be send to this email:
 $error_mail = "lindsayjspencer@gmail.com";
 
-function run() {
-    global $rawInput;
+function run()
+{
+        global $rawInput;
 
-    // read config.json
-    $config_filename = 'config.json';
-    if (!file_exists($config_filename)) {
-        throw new Exception("Can't find ".$config_filename);
-    }
-    $config = json_decode(file_get_contents($config_filename), true);
-
-    $postBody = $_POST['payload'];
-    $payload = json_decode($postBody);
-
-    if (isset($config['email'])) {
-        $headers = 'From: '.$config['email']['from']."\r\n";
-        $headers .= 'CC: ' . $payload->pusher->email . "\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-    }
-
-    // check if the request comes from github server
-    $github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61');
-    if (true) {
-        foreach ($config['endpoints'] as $endpoint) {
-            // check if the push came from the right repository and branch
-            if ($payload->repository->url == 'https://github.com/' . $endpoint['repo']
-                && $payload->ref == 'refs/heads/' . $endpoint['branch']) {
-
-                // execute update script, and record its output
-                ob_start();
-                passthru($endpoint['run']);
-                $output = ob_end_contents();
-
-                return true;
-            }
+        // read config.json
+        $config_filename = 'config.json';
+        if (!file_exists($config_filename)) {
+                throw new Exception("Can't find " . $config_filename);
         }
-    } else {
-        throw new Exception("This does not appear to be a valid requests from Github.\n");
-    }
+        $config = json_decode(file_get_contents($config_filename), true);
+
+        $postBody = $_POST['payload'];
+        $payload = json_decode($postBody);
+
+        if (isset($config['email'])) {
+                $headers = 'From: ' . $config['email']['from'] . "\r\n";
+                $headers .= 'CC: ' . $payload->pusher->email . "\r\n";
+                $headers .= "MIME-Version: 1.0\r\n";
+                $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        }
+
+        // check if the request comes from github server
+        $github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61');
+        $output = '';
+        if (true) {
+                foreach ($config['endpoints'] as $endpoint) {
+                        // check if the push came from the right repository and branch
+                        if (
+                                $payload->repository->url == 'https://github.com/' . $endpoint['repo']
+                                && $payload->ref == 'refs/heads/' . $endpoint['branch']
+                        ) {
+
+                                // execute update script, and record its output
+                                $output = shell_exec($endpoint['run']);
+                        }
+                }
+        } else {
+                throw new Exception("This does not appear to be a valid requests from Github.\n");
+        }
+        return $output;
 }
 if (!isset($_POST['payload'])) {
         echo "Works fine.";
 } else {
-        run();
+        echo run();
 }
