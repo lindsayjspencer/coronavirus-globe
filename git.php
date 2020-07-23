476b4fdaccfc755b673de912187e0e83597e3108
Lindsay Spencer <lindsayjspencer@gmail.com>
2020-07-23 19:15:39 +1000

    Remove rand()

--- git.php
+++ git.php
@@ -5,6 +5,6 @@ $log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
         "Payload: ".$_POST["PAYLOAD"].PHP_EOL.
         "-------------------------".PHP_EOL;
 //Save string to log, use FILE_APPEND to append.
-file_put_contents('./logs/log_'.date("j.n.Y").rand().'.log', $log, FILE_APPEND);
+file_put_contents('./logs/log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
 
 ?>
